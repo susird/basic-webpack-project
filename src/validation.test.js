9bd04eec966b690e8d69d2491e0883fe658654f4
susana <susir95@gmail.com>
2021-07-19 00:32:22 -0500

    adding test jest to validation

--- src/validation.test.js
+++ src/validation.test.js
@@ -4,6 +4,26 @@ test('should return: [Name is required]', () => {
   const userName = '';
   const password = 'sjkfjsdkf';
   const message = validation(userName, password);
-  console.log(message)
   expect(message).toContain('Name is required');
+})
+
+test('should return: [Password cannot be password]', () => {
+  const userName = 'fsdfsdfs';
+  const password = 'password';
+  const message = validation(userName, password);
+  expect(message).toContain('Password cannot be password');
+})
+
+test('should return: [Password must be longer than 6 characters]', () => {
+  const userName = 'fsdfsdfs';
+  const password = ([0-6]);
+  const message = validation(userName, password);
+  expect(message).toContain('Password must be longer than 6 characters');
+})
+
+test('should return: [Password must be longer than 6 characters]', () => {
+  const userName = 'fsdfsdfs';
+  const password = ([20-999]);
+  const message = validation(userName, password);
+  expect(message).toContain('Password must be longer than 6 characters');
 })
